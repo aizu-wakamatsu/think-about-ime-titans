a9a2db008ecfebbeb5be404289ce64f027d19cc4
tari <aizuwakamatsu@outlook.jp>
2023-10-30 23:57:33 +0900

    Shinchoku

--- app/src/main/java/com/example/ime_titans/keyboard.kt
+++ app/src/main/java/com/example/ime_titans/keyboard.kt
@@ -1,16 +1,13 @@
 package com.example.ime_titans
 
 import android.inputmethodservice.InputMethodService
-import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import android.widget.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ime_titans.ui.theme.ImetitansTheme
 
@@ -29,28 +26,96 @@ class keyboard : InputMethodService() {
 //            }
 //        }
 //    }
+
+    private val keymap: KeymapHolder = Keymap()
+    private var currentKeyId = 0
     override fun onCreateInputView(): View {
-        return layoutInflater.inflate(R.layout.input, null).apply { //res/layout/input.xml
-            if (this is MyKeyboardView) {
-                setOnKeyboardActionListener(this)
-//                keyboard = latinKeyboard
-            }
+    return layoutInflater.inflate(R.layout.keyboard, null).also { view ->
+        keymap.keys.map { id ->
+            view.findViewById<Button>(id).also { it.setOnTouchListener(this) }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ImetitansTheme {
-        Greeting("Android")
     }
+
+//    private fun setOnKeyboardActionListener(keyboard: keyboard) {
+//
+//    }
+override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+    return if (v is Button && event is MotionEvent) {
+        when (event.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
+                // 押下したボタンのandroid:idを保持
+                currentKeyId = v.id
+                false
+            }
+            MotionEvent.ACTION_UP -> {
+                // 押下ボタンに基づいた文字を入力
+                sendKeyEvent(currentKeyId)
+                currentKeyId = 0
+                false
+            }
+            // タッチ位置が変わったり、複数指でタッチしても
+            // 見た目の変化が発生しないようにする
+            else -> true
+        }
+    } else {
+        true
+    }
+}
+
+    // キー入力。本来はもっとごちゃごちゃするので簡略化。
+    private fun sendKeyEvent(id: Int) {
+        if (id !in keymap.keys) {
+            return
+        }
+
+        val keyInfo = keymap.getKeyInfo(id)
+        if (keyInfo is KeyInfo.AsciiKeyInfo) {
+            sendKeyChar(keyInfo.char)
+        }
+    }
+
+}
+interface KeymapHolder {
+    val keys: Set<Int>
+    fun getKeyInfo(indexId: Int): KeyInfo
+}
+
+class Keymap : KeymapHolder {
+    private val list: Map<Int, KeyInfo> = mapOf(
+        R.id.key_1 to KeyInfo.Num1,
+        R.id.key_2 to KeyInfo.Num2,
+        R.id.key_3 to KeyInfo.Num3,
+        R.id.key_4 to KeyInfo.Num4,
+        // 中略
+        R.id.key_19 to KeyInfo.Num9,
+        R.id.key_20 to KeyInfo.Num0
+    )
+
+    override val keys = list.keys
+
+    override fun getKeyInfo(indexId: Int): KeyInfo {
+        return list.getOrDefault(indexId, KeyInfo.Null)
+    }
+}
+
+sealed class KeyInfo {
+    object Null : KeyInfo()
+
+    abstract class AsciiKeyInfo : KeyInfo() {
+        abstract val char: Char
+        abstract val code: Int
+    }
+
+    object Num0 : AsciiKeyInfo() {
+        override val char = '0'
+        override val code = KeyEvent.KEYCODE_0
+    }
+
+    object Num1 : AsciiKeyInfo() {
+        override val char = '1'
+        override val code = KeyEvent.KEYCODE_1
+    }
+
+    // 以下略
 }
---
+++ docs/0x-reference.md
@@ -0,0 +1,12 @@
+# 0x-reference
+
+
+
+## 参考資料
+
+資料へのURLは2023年10月時点のものです。
+
+@Dooteeen(2020). AndroidX時代のIME作成 (1: 簡単なキーボードの作成). Qiita
+
+https://qiita.com/Dooteeen/items/d32446be401096c75712
+
